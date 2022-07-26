a36b1c11b3a9679281bf9d88a4b8b23e999c8a69
zlauzon <9244781+zlauzon@users.noreply.github.com>
2022-07-26 16:17:47 -0600

    document solution

--- palindrome-number-solution.cpp
+++ palindrome-number-solution.cpp
@@ -7,16 +7,16 @@ class Solution {
 public:
     bool isPalindrome(int x) {
         bool flag = false;
-        if (x < 0) return flag;
+        if (x < 0) return flag; //disqualify all negative numbers
         int lastDigit;
         long long reverse = 0;
         auto num = x;
         while (num) {
-            lastDigit = num % 10;
-            reverse = (reverse * 10) + lastDigit;
-            num /= 10;
+            lastDigit = num % 10; //copy digit from the rear of the number
+            reverse = (reverse * 10) + lastDigit; //add copied digit to the front of the number
+            num /= 10; //remove copied digit from working number
         }
-        flag = (reverse == x) ? true : false;
+        flag = (reverse == x) ? true : false; //if our reverse number equals the passed number it is a palindrome
         return flag;
     }
 };
